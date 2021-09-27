d3f04009cd197165211e7d110b1ace4728e95324
vin-spiegel <turbogara1@gmail.com>
2021-09-27 10:50:45 +0900

    add ScrollPanel/gridPanel

--- Scripts/__UIManager.lua
+++ Scripts/__UIManager.lua
@@ -9,6 +9,11 @@ Control = {
     showOnTop = false
 }
 
+--멤버함수
+function Control:Add(parent, child)
+    parent.AddChild(child)
+end
+
 --자식 클래스 생성
 function Control:init(obj)
     setmetatable(obj, self)
@@ -16,7 +21,7 @@ function Control:init(obj)
     return obj
 end
 
---패널 기본값 설정
+--패널 기본값
 panel =
     Control:init {
     width = Client.width * 0.35,
@@ -36,7 +41,62 @@ function panel:new(obj)
     return temp
 end
 
---버튼객체 기본값 설정
+--그리드패널 기본값
+gridPanel =
+    Control:init {
+    width = 100,
+    height = 100,
+    cellSize = Point(100, 100),
+    horizontal = true,
+    vertical = true
+}
+function gridPanel:new(obj)
+    setmetatable(obj, self)
+    self.__index = self
+
+    local temp = Image(obj.path, Rect(obj.x, obj.y, obj.width, obj.height))
+
+    temp.pivotX, temp.pivotY = obj.pivotX, obj.pivotY
+    temp.anchor = obj.anchor
+    temp.showOnTop = obj.showOnTop
+
+    gridPanel.cellSize = obj.cellSize
+    gridPanel.horizontal = obj.horizontal
+    gridPanel.vertical = obj.vertical
+
+    return temp
+end
+
+--스크롤패널 기본값
+scrollPanel =
+    Control:init {
+    width = 100,
+    height = 100,
+    masked = true,
+    horizontal = true,
+    vertical = true,
+    showHorizontalScrollbar = true,
+    showVerticalScrollbar = true
+}
+function scrollPanel:new(obj)
+    setmetatable(obj, self)
+    self.__index = self
+
+    local temp = Image(obj.path, Rect(obj.x, obj.y, obj.width, obj.height))
+
+    temp.pivotX, temp.pivotY = obj.pivotX, obj.pivotY
+    temp.anchor = obj.anchor
+    temp.showOnTop = obj.showOnTop
+
+    gridPanel.horizontal = obj.horizontal
+    gridPanel.vertical = obj.vertical
+    gridPanel.showHorizontalScrollbar = obj.showHorizontalScrollbar
+    gridPanel.showVerticalScrollbar = obj.showVerticalScrollbar
+
+    return temp
+end
+
+--버튼객체 기본값
 button =
     Control:init {
     width = 80,
@@ -62,7 +122,7 @@ function button:new(obj)
     return temp
 end
 
---이미지객체 기본값 설정
+--이미지객체 기본값
 image =
     Control:init {
     path = "폴더/파일명.png",
@@ -81,15 +141,9 @@ function image:new(obj)
     temp.pivotX, temp.pivotY = obj.pivotX, obj.pivotY
     temp.anchor = obj.anchor
     temp.showOnTop = obj.showOnTop
-
     temp.imageType = obj.imageType
     temp.fillMethod = obj.fillMethod
     temp.fillClockwise = obj.fillClockwise
 
     return temp
 end
-
---test
-mainPanel = panel:new {}
-mainbtn = button:new {text = "x"}
-img = image:new {}
---
+++ Scripts/testUI.lua
@@ -0,0 +1,1 @@
+local mainPanel = panel:new {}
