7f0818939cb5c45e84ae062a6768aaad117b8129
vin-spiegel <turbogara1@gmail.com>
2021-09-27 09:40:44 +0900

    test

---
+++ Scripts/__UIManager.lua
@@ -0,0 +1,67 @@
+--컨트롤 클래스 & 기본 설정값
+Control = {
+    x = 0,
+    y = 0,
+    pivotX = 0.5,
+    pivotY = 0.5,
+    anchor = 4,
+    color = Color(0, 0, 0, 100),
+    showOnTop = false
+}
+
+--자식 클래스 생성
+function Control:init(obj)
+    setmetatable(obj, self)
+    self.__index = self
+    return obj
+end
+
+--패널 기본값 설정
+panel =
+    Control:init {
+    width = Client.width * 0.35,
+    height = Client.height * 0.75
+}
+function panel:new(obj)
+    setmetatable(obj, self)
+    self.__index = self
+
+    local temp = Panel(Rect(obj.x, obj.y, obj.width, obj.height))
+
+    temp.pivotX, temp.pivotY = obj.pivotX, obj.pivotY
+    temp.anchor = obj.anchor
+    temp.showOnTop = obj.showOnTop
+    temp.color = obj.color
+
+    return temp
+end
+
+--버튼객체 기본값 설정
+button =
+    Control:init {
+    width = 80,
+    height = 50,
+    color = Color(255, 255, 255),
+    text = "",
+    textColor = Color(0, 0, 0),
+    textSize = 16
+}
+function button:new(obj)
+    setmetatable(obj, self)
+    self.__index = self
+
+    local temp = Button(obj.text, Rect(obj.x, obj.y, obj.width, obj.height))
+
+    temp.pivotX, temp.pivotY = obj.pivotX, obj.pivotY
+    temp.anchor = obj.anchor
+    temp.showOnTop = obj.showOnTop
+    temp.color = obj.color
+    temp.textSize = obj.textSize
+    temp.textColor = obj.textColor
+
+    return temp
+end
+
+--test
+mainPanel = panel:new {}
+mainbtn = button:new {text = "x"}
