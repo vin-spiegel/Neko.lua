d8ae07e3e5ef47c41330fcb5fde01c59e06235cf
vin-spiegel <turbogara1@gmail.com>
2021-09-29 10:07:55 +0900

    wait,tableExtension

--- ServerScripts/__methods.lua
+++ ServerScripts/__methods.lua
@@ -14,7 +14,7 @@ RunLater = function(obj)
         parameters[i - 1] = obj[i]
     end
 
-    Client.RunLater(
+    Server.RunLater(
         function()
             _func(table.unpack(parameters))
         end,
@@ -32,3 +32,23 @@ RunLater {
     "c",
     time = 2
 }
+
+--wait함수
+function wait(time)
+    local callTime = os.clock() + (time / 1000)
+    repeat
+    until os.clock() > callTime
+end
+
+--table메소드 익스텐션
+function table.print(t)
+    if not t then
+        return false
+    end
+
+    print("Table = \n{")
+    for k, v in pairs(t) do
+        print(string.format("\t%s = %s%s", k, v, (k == #t and "") or ","))
+    end
+    print("}")
+end
