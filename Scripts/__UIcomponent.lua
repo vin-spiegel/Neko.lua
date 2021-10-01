f00586e95e3ca382e9837a657d050d39ccd90654
vin-spiegel <turbogara1@gmail.com>
2021-10-02 04:42:18 +0900

    edit:rect관련 함수추가

--- Scripts/__UIcomponent.lua
+++ Scripts/__UIcomponent.lua
@@ -5,7 +5,7 @@ local Control = {
     prototype = {
         x = 0,
         y = 0,
-        xy = Point(0, 0),
+        xy = {0, 0},
         width = 100,
         height = 100,
         rect = Rect(0, 0, 100, 100),
@@ -17,57 +17,79 @@ local Control = {
         anchor = 4,
         showOnTop = false,
         visible = true
-    },
-    xy = function(userdata, v)
-        userdata.x = v.x
-        userdata.y = v.y
-        print("xy 호출 : ", userdata.x, userdata.y)
-    end,
-    height = 100,
-    rect = Rect(0, 0, 100, 100),
-    --
-    pivotX = 0.5,
-    pivotY = 0.5,
-    pivot = Point(0.5, 0.5),
-    --
-    anchor = 4,
-    showOnTop = false,
-    visible = true
+    }
 }
 
---자식 클래스 생성 메소드
-function Control:class(obj)
-    setmetatable(obj, self)
-    self.__index = self
-    return obj
-end
-
+--##########
 --멤버함수모음
+--##########
+
 --Rect관련
 
-function Control:Rect(n)
-    print("Rect 호출")
+--Rect get/set
+function Control:Rect(x, y, width, height)
+    local userdata = self.obj
+    if type(x) == "number" and type(y) == "number" and type(width) == "number" and type(height) == "number" then
+        --set
+        userdata.rect = Rect(x, y, width, height)
+        return self
+    elseif not x and not y and not width and not height then
+        --get
+        return Rect(userdata.x, userdata.y, userdata.width, userdata.height)
+    else
+        print("error : 인자 값이 잘못되었습니다.")
+    end
+end
+
+--height get/set
+function Control:height(n)
+    local userdata = self.obj
+    if not n then
+        return userdata.height
+    elseif type(n) == "number" then
+        userdata.height = n
+        return self
+    end
+end
+
+--x,y point get/set
+--@param table point
+--@returns userData Point
+function Control:xy(point)
+    local userdata = self.obj
+    if not point then
+        return Point(userdata.x, userdata.y)
+    elseif type(point) == "table" then
+        userdata.x = point[1]
+        userdata.y = point[2]
+        return self
+    end
 end
 
 --객체 set 함수
-function Control:set(obj)
-    local userdata = self[1] or obj[1]
-    if not userdata then
-        print("Error: 먼저 obj:new{}로 객체를 생성해주세요.")
+function Control:set(var, inst)
+    local userdata
+    --최초 생성
+    if not var then
+        print("Error: 값이 없습니다.")
         return
+    elseif inst then
+        --inst에 객체가 있을때
+        for key, v in pairs(var) do
+            if inst[key] then
+                inst[key](inst, v)
+            end
+        end
+        return inst
     end
 
-    --userdata 객체 값 세팅
-    for key, v in pairs(obj) do
-        if self[key] and type(self[key]) == "function" then
-            print("함수실행")
-            self[key](userdata, v)
-        elseif self[key] then
-            userdata[key] = v
+    --self에 객체가 있을때
+    for key, v in pairs(var) do
+        if self[key] then
+            self[key](self, v)
         end
     end
-
-    table.print(self)
+    return self
 end
 
 --네코 객체 생성
@@ -94,45 +116,64 @@ function Control:new(var)
 
     --인스턴스에 obj키를 가지는 유저데이터 넣어주기
     inst.obj = userdata
-    return inst
+
+    --들어온 var기준으로 set
+    return self:set(var, inst)
 end
 
---전역 클래스 정의
---패널 클래스
-panel =
-    Control:class {
-    type = "Panel",
-    color = Color(0, 0, 0, 150),
-    --유저데이터 생성 함수
-    newUserData = function()
-        local temp = Panel()
-        temp.color = panel.color
-        return temp
-    end
-}
-function Control:test()
-    print("테스트함수 호출")
-    return self
-end
-
+--가로 길이 get/set
 function Control:width(n)
     local userdata = self.obj
     if not n then
         return userdata.width
-    else
+    elseif type(n) == "number" then
         userdata.width = n
         return self
     end
 end
+
+--자식 클래스 생성 메소드
+function Control:class(obj)
+    setmetatable(obj, self)
+    self.__index = self
+    return obj
+end
+
+--#############
+--전역 클래스 정의
+--#############
+
+--패널 클래스
+panel =
+    Control:class {
+    type = "Panel",
+    --유저데이터 생성 함수
+    newUserData = function()
+        local temp = Panel()
+        --기본값 세팅
+        temp.color = Color(0, 0, 0, 155)
+        return temp
+    end
+}
+
+button =
+    Control:class {
+    type = "Button",
+    --유저데이터 생성 함수
+    newUserData = function()
+        local temp = Button()
+        --기본 값 세팅
+        temp.color = Color(0, 0, 0, 255)
+        return temp
+    end
+}
+
 --테스트 코드
 Client.RunLater(
     function()
-        a =
-            panel:new {
-            rect = Rect(11, 44, 200, 150),
-            xy = Point(22, 33)
-        }:test():width(100)
-        print(a.type)
+        test = panel:new {width = 50, xy = {12, 34}}
+        -- test:set {}:Rect(20, 30, 150, 200)
+        -- test:width(50):height(50)
     end,
-    2
+    1
 )
