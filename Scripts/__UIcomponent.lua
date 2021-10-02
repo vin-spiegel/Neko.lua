7befbadc5c9b3ca2c7adad8e373d55c3395997dd
vin-spiegel <turbogara1@gmail.com>
2021-10-02 18:01:20 +0900

    edit:class구조 리팩토링

--- Scripts/__UIcomponent.lua
+++ Scripts/__UIcomponent.lua
@@ -1,29 +1,85 @@
 --##UI생성 컴포넌트
 
---컨트롤 클래스 (공용 기본값)
+--##컨트롤 클래스
 local Control = {
-    prototype = {
-        x = 0,
-        y = 0,
-        xy = {0, 0},
-        width = 100,
-        height = 100,
-        rect = Rect(0, 0, 100, 100),
-        --
-        pivotX = 0.5,
-        pivotY = 0.5,
-        pivot = Point(0.5, 0.5),
-        --
-        anchor = 4,
-        showOnTop = false,
-        visible = true
-    }
+    --프로퍼티
+    children = function(self)
+    end,
+    parent = function(self)
+    end,
+    showOnTop = function(self)
+    end,
+    orderIndex = function(self, bool)
+    end,
+    --##멤버함수##
+    set = function(self, var)
+        print("set함수 발동")
+        local tablekeys = function()
+            local n = 0
+            for _, _ in pairs(var) do
+                n = n + 1
+            end
+            return n
+        end
+
+        --설정할 프로퍼티 값이 없을 때
+        if not var or (type(var) == "table" and tablekeys() == 0) then
+            return self
+        end
+
+        --객체에 프로퍼티 값 설정
+        for key, v in pairs(var) do
+            if self[key] then
+                self[key](self, v)
+            end
+        end
+        return self
+    end,
+    new = function(self, var)
+        --userdata객체가 없을때 새로 생성
+        if self.obj then
+            print("error : 이미 객체가 있습니다.")
+            return
+        end
+        --들어온 테이블에 Control 클래스 연결
+        local inst = setmetatable({}, self)
+        self.__index = self
+
+        --유저데이터 타입 객체 생성
+        local userdata = self.newUserData()
+
+        --인스턴스에 obj키를 가지는 유저데이터 넣어주기
+        inst.obj = userdata
+
+        --들어온 var기준으로 set
+        local a = self.set(inst, var)
+        return a
+    end,
+    attachToUnit = function(self)
+    end,
+    attachToUnitID = function(self)
+    end,
+    clone = function(self)
+    end,
+    destroy = function(self)
+    end,
+    removeChild = function(self)
+    end,
+    orderToFirst = function(self)
+    end,
+    orderToLast = function(self)
+    end,
+    --New
+    class = function(self, obj)
+        setmetatable(obj, self)
+        self.__index = self
+        return obj
+    end
 }
 
---##########
---멤버함수정의
---##########
-
+--#############
+--전역 클래스 정의
+--#############
 --Rect관련
 --Rect get/set
 function Control:Rect(x, y, width, height)
@@ -53,8 +109,6 @@ function Control:rect(rec)
 end
 
 --x,y point get/set
---@param table point
---@returns userData Point
 function Control:xy(x, y)
     local userdata = self.obj
     if not x and not y then
@@ -166,95 +220,57 @@ function Control:anchor(n)
     end
 end
 
---color get/set
-function Control:color(r, g, b, a)
-    local userdata = self.obj
-
-    if not n then
-        --get
-        return userdata.color
-    elseif type(r) == "number" and type(g) == "number" and type(b) == "number" then
-        --set
-        if type(a) == "number" then
-            userdata.color = Color(r, g, b, a)
-        else
-            userdata.color = Color(r, g, b)
-        end
-        return self
-    end
-end
-
---객체 set 함수
-function Control:set(var)
-    local tablekeys = function()
-        local n = 0
-        for _, _ in pairs(var) do
-            n = n + 1
-        end
-        return n
-    end
-
-    if not var or (type(var) == "table" and tablekeys() == 0) then
-        -- print("Error: 설정할 프로퍼티 값이 없습니다.")
-        return self
-    end
-
-    --객체에 프로퍼티 값 설정
-    for key, v in pairs(var) do
-        if self[key] then
-            self[key](self, v)
-        end
-    end
-    return self
-end
-
---네코 객체 생성
-
---객체 생성및 최초값 설정
-function Control:new(var)
-    --userdata객체가 없을때 새로 생성
-    if self.obj then
-        print("error : 이미 객체가 있습니다.")
-        return
-    end
-    --들어온 테이블에 Control 클래스 연결
-    local inst = setmetatable({}, self)
-    self.__index = self
-
-    --유저데이터 타입 객체 생성
-    local userdata = self.newUserData()
-
-    --인스턴스에 obj키를 가지는 유저데이터 넣어주기
-    inst.obj = userdata
-
-    --들어온 var기준으로 set
-    local a = self.set(inst, var)
-    return a
-end
-
---자식 클래스 생성 메소드
-function Control:class(obj)
-    setmetatable(obj, self)
-    self.__index = self
-    return obj
-end
-
---#############
---전역 클래스 정의
---#############
-
---패널 클래스
+--##패널 클래스
 panel =
     Control:class {
+    --##프로퍼티
     type = "Panel",
-    --유저데이터 생성 함수
+    color = function(self, r, g, b, a)
+        table.print(self)
+        return self
+    end,
+    masked = function(self)
+        table.print(self)
+        return self
+    end,
+    --함수
+    loadPage = function(self)
+        print(self)
+        return self
+    end,
     newUserData = function()
         local temp = Panel()
         --기본값 세팅
         temp.color = Color(0, 0, 0, 155)
         return temp
+    end,
+    new = function(self, var)
+        --userdata객체가 없을때 새로 생성
+        if self.obj then
+            print("error : 이미 객체가 있습니다.")
+            return
+        end
+        --들어온 테이블에 Control 클래스 연결
+        local inst = setmetatable({}, self)
+        self.__index = self
+
+        --유저데이터 타입 객체 생성
+        local userdata = Panel()
+
+        --인스턴스에 obj키를 가지는 유저데이터 넣어주기
+        inst.obj = userdata
+
+        --들어온 var기준으로 set
+        local a = self.set(inst, var)
+        return a
     end
 }
+
+--####테스트 코드
+function testpanel()
+    local a = panel:new {}
+end
+Client.RunLater(testpanel, 1)
 
 --##button Class
 button =
@@ -265,6 +281,7 @@ button =
         return Button()
     end
 }
+
 function button:onClick(_func)
     local userdata = self.obj
 
@@ -333,7 +350,6 @@ color =
     end
 }
 function color:color(r, g, b, a)
-    print("칼라객체 접근")
     local obj
 
     if type(r) == "number" and type(g) == "number" and type(b) == "number" then
@@ -407,7 +423,7 @@ end
 
 function testCode()
     -- local a = button:new {text = "안녕하세요 버튼 텍스트"}
-    local b = text:new {text = "안녕하세요 텍스트"}:color(255, 2555, 3)
+    -- local b = text:new {text = "안녕하세요 텍스트"}:color(255, 2555, 3)
     -- local a = color:new {}:color()
     -- print(a)
 end
